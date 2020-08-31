7a814a8c6a0167f174331bbf6402537fb36d5688
Martin Kunz <martin.michael.kunz@gmail.com>
2020-08-31 09:58:23 +0200

    send dmx

--- src/main.cpp
+++ src/main.cpp
@@ -2,39 +2,17 @@
 #include <JC_Button.h>
 #include <EEPROM.h>
 #include <SPI.h>
-#include "ModbusRtu.h"
 #include "light.h"
 #include "room.h"
-
+#include "terminal.h"
 #include <string.h>
-
-// For any Modbus slave devices are reserved addresses in the range from 1 to 247.
-// Important note only address 0 is reserved for a Modbus master device!
-#define MasterModbusAdd  0
-#define SlaveModbusAdd  1
-
-
-// This MACRO defines number of the comport that is used for RS 485 interface.
-// For MAXI and MEGA RS485 is reserved UART Serial3.
-#define RS485Serial     3
-// The object ControllinoModbuSlave of the class Modbus is initialized with three parameters.
-// The first parametr specifies the address of the Modbus slave device.
-// The second parameter specifies type of the interface used for communication between devices - in this sketch is used RS485.
-// The third parameter can be any number. During the initialization of the object this parameter has no effect.
-Modbus ControllinoModbusMaster(MasterModbusAdd, RS485Serial, 0);
-
-// This is an structe which contains a query to an slave device
-modbus_t ModbusQuery;
-uint16_t ModbusSlaveRegisters[8];
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+#include <NetEEPROM.h>
+#include <dmx.h>
 
 
-uint8_t myState; // machine state
-uint8_t currentQuery; // pointer to message query
-
-unsigned long WaitingTime;
-
-
-
+EthernetUDP Udp;
 
 Room room[] = {
   Room(1, "Salon"),
@@ -51,60 +29,49 @@ Light lights[] = {
  Light(4, 2,false, CONTROLLINO_A1, CONTROLLINO_D11, "Lampe") 
 };
 
+Terminal terminals[] = {
+ Terminal(1, 1,"SalonTerminal"),
+};
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
+
 void setup()
 {
   Serial.begin(19200);
   Serial.println("-----------------------------------------");
-  Serial.println("CONTROLLINO Modbus RTU Master Test Sketch");
-  Serial.println("-----------------------------------------");
   Serial.println("");
-    // ModbusQuery 0: read registers
-  ModbusQuery.u8id = SlaveModbusAdd; // slave address
-  ModbusQuery.u8fct = 3; // function code (this one is registers read)
-  ModbusQuery.u16RegAdd = 0; // start address in slave
-  ModbusQuery.u16CoilsNo = 4; // number of elements (coils or registers) to read
-  ModbusQuery.au16reg = ModbusSlaveRegisters; // pointer to a memory array in the CONTROLLINO
-
-  ControllinoModbusMaster.begin( 19200 ); // baud-rate at 19200
-  ControllinoModbusMaster.setTimeOut( 1000 ); // if there is no answer in 5000 ms, roll over
-  
-  WaitingTime = millis() + 1000;
-  myState = 0;
-  currentQuery = 0; 
-
   for(int i=0; i<(sizeof(lights)/sizeof(*lights)); i++) {
     lights[i].inputNr.begin();
   }
-    // Instantiate network from stored netowrk paramters in EEPROM.
-  // If not in EEPROM then a random MAC will be generated and stored in EEPROM
-  // in the form DE:AD:BE:EF:xx:xx where xx is a random number from 0 to 255.
-  // If the netowrk is not configured in EEPROM, then DHCP will be used.
+  // NetEeprom.begin();
+  // Udp.begin(1717);
+  dmx_init();
 }
 
-
-
-
+int dir=1;
+int cnt=0;
 void loop() {
-    ControllinoModbusMaster.query( ModbusQuery ); // send query (only once)
-    ControllinoModbusMaster.poll(); // check incoming messages
-    if (ControllinoModbusMaster.getState() == COM_IDLE) 
-      {
-        // response from the slave was received
-        myState = 0;
-        WaitingTime = millis() + 1000; 
-        // registers read was proceed
-        Serial.println("---------- READ RESPONSE RECEIVED ----");
-        Serial.print("Slave ");
-        Serial.print(SlaveModbusAdd, DEC); 
-        Serial.print(" ADC0: 0x");
-        Serial.print(ModbusSlaveRegisters[0], HEX);
-        Serial.print(" , Digital0: ");
-        Serial.print(ModbusSlaveRegisters[1], BIN);
-        Serial.print(" , ModbusCounterIn: ");
-        Serial.print(ModbusSlaveRegisters[2], DEC);
-        Serial.print(" , ModbusCounterOut: ");
-        Serial.println(ModbusSlaveRegisters[3], DEC);
-        Serial.println("-------------------------------------");
-        Serial.println("");
-      }
+
+  cnt++;
+  if(cnt%1000==0) {
+  	dmx_buffer[1]+=dir;
+    if(dmx_buffer[1]>=50)
+      dir=-1;
+    if(dmx_buffer[1]==0)
+      dir=1;
+  }
+
+
+
+
+//   int packetSize = Udp.parsePacket();
+//   if(packetSize)
+//   {
+//     IPAddress remote = Udp.remoteIP();
+//     uint16_t port = Udp.remotePort();
+//     // read the packet into packetBufffer
+//     Udp.read(packetBuffer,UDP_TX_PACKET_MAX_SIZE);
+//     Udp.beginPacket(remote, port);
+//     Udp.write("OK");
+//     Udp.endPacket();
+// }
 }
---
+++ src/terminal.h
@@ -0,0 +1,18 @@
+#include <Controllino.h>
+#include <JC_Button.h>
+
+
+class Terminal;
+class Terminal {
+   public:
+     uint8_t nr;
+     uint8_t RoomNr;
+     const char *name;
+
+   
+  Terminal(uint8_t _nr, uint8_t _RoomNr, const char *_name): 
+  nr(_nr),
+  RoomNr(_RoomNr),
+  name(_name) {
+  }
+};
