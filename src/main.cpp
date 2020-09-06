d7ac8ce35671169c2f74b9b978c6edf6481ace50
Martin Kunz <martin.michael.kunz@gmail.com>
2020-09-06 16:57:45 +0200

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -11,23 +11,23 @@
 #include <EthernetUdp.h>
 #include <NetEEPROM.h>
 #include <dmx.h>
-extern "C" void vConfigureTimerForRunTimeStats( void ) {
+#include <utility/w5100.h>
+extern "C" void vConfigureTimerForRunTimeStats(void)
+{
 }
 
-extern "C" unsigned long vGetTimerForRunTimeStats( void ) {
-    return micros();
+extern "C" unsigned long vGetTimerForRunTimeStats(void)
+{
+  return micros();
 }
 
 #include <Arduino_FreeRTOS.h>
 #include <task.h>
 #include <timers.h>
 
-
 #define TLight 1
 #define TDim 2
-#define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
-
-
+#define COUNT_OF(x) ((sizeof(x) / sizeof(0 [x])) / ((size_t)(!(sizeof(x) % sizeof(0 [x])))))
 
 EthernetUDP Udp;
 void handlePacket();
@@ -35,42 +35,40 @@ void timer_init();
 void printTasks();
 void TaskMixer(void *pvParameters);
 void TaskNetwork(void *pvParameters);
- void vMixerTimerCallback( TimerHandle_t xTimer );
-
+void vMixerTimerCallback(TimerHandle_t xTimer);
 
 Room room[] = {
-  Room(1, "Salon"),
-  Room(2, "Miranda"),
-  Room(3, "Kueche"),
-  Room(4, "BadUnten"),
-  Room(5, "BadOben"),
+    Room(1, "Salon"),
+    Room(2, "Miranda"),
+    Room(3, "Kueche"),
+    Room(4, "BadUnten"),
+    Room(5, "BadOben"),
 };
 
 Light lights[] = {
- Light(1, 1,false, CONTROLLINO_A0, CONTROLLINO_D11, "Lampe"),
- Light(2, 2,false, CONTROLLINO_A1, CONTROLLINO_D11, "Lampe"),
- Light(3, 2,false, CONTROLLINO_A1, CONTROLLINO_D11, "Lampe"),
- Light(4, 2,false, CONTROLLINO_A1, CONTROLLINO_D11, "Lampe") 
-};
+    Light(1, 1, false, CONTROLLINO_A0, CONTROLLINO_D11, "Lampe"),
+    Light(2, 2, false, CONTROLLINO_A1, CONTROLLINO_D11, "Lampe"),
+    Light(3, 2, false, CONTROLLINO_A1, CONTROLLINO_D11, "Lampe"),
+    Light(4, 2, false, CONTROLLINO_A1, CONTROLLINO_D11, "Lampe")};
 
 Dimmer dimmers[] = {
- Dimmer(1, 1,1,2,"Leds am Tisch1"),
-  Dimmer(2, 1,3,4,"Leds am Tisch2")
+    Dimmer(1, 1, 1, 2, "Leds am Tisch1"),
+    Dimmer(2, 1, 3, 4, "Leds am Tisch2")
 
 };
 
 Terminal terminals[] = {
- Terminal(1, 1,"SalonTerminal"),
+    Terminal(1, 1, "SalonTerminal"),
 };
 char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
-
 
 TaskHandle_t taskNetworkHandle;
 TimerHandle_t timerMixerHandler;
 
 void setup()
 {
-  for(int i=0; i<COUNT_OF(lights); i++) {
+  for (int i = 0; i < COUNT_OF(lights); i++)
+  {
     lights[i].inputNr.begin();
   }
   dmx_init();
@@ -78,174 +76,150 @@ void setup()
   Serial.println("--IODUB--");
   NetEeprom.begin();
   Udp.begin(1717);
-timerMixerHandler = xTimerCreate
-                   ( /* Just a text name, not used by the RTOS
-                     kernel. */
-                     "Mixer",
-                     /* The timer period in ticks, must be
-                     greater than 0. */
-                     2,
-                     /* The timers will auto-reload themselves
-                     when they expire. */
-                     pdTRUE,
-                     /* The ID is used to store a count of the
-                     number of times the timer has expired, which
-                     is initialised to 0. */
-                     ( void * ) 0,
-                     /* Each timer calls the same callback when
-                     it expires. */
-                     vMixerTimerCallback
-                   );
-  // xTimerStart(timerMixerHandler,0);
-
-  xTaskCreate(TaskNetwork, // Task function
-              "Network", // Task name
-              128, // Stack size 
-              NULL, 
-              0, // Priority
+  timerMixerHandler = xTimerCreate("Mixer", 2, pdTRUE,
+                                   (void *)0,
+                                   vMixerTimerCallback);
+  xTimerStart(timerMixerHandler,0);
+  xTaskCreate(TaskNetwork, "Network", 
+              128,         // Stack size
+              NULL,
+              0,                   // Priority
               &taskNetworkHandle); // Task handler
 }
-
-int dir=1;
-int cnt=0;
-unsigned long time=0;
-unsigned long td=0;
-
-int8_t sgn(int8_t x) {
-  if (x > 0) return 1;
-  if (x < 0) return -1;
-  return 0;
-}
-
- void vMixerTimerCallback( TimerHandle_t xTimer )
- {
-    for(uint8_t i=0;i!=DMX_CHANNELS;i++) {
-      int16_t diff=dmx_set[i]-dmx_cur[i];
-      dmx_cur[i]=dmx_cur[i]+diff;
-    }
+void vMixerTimerCallback(TimerHandle_t xTimer)
+{
+  for (uint8_t i = 0; i != DMX_CHANNELS; i++)
+  {
+    int16_t diff = dmx_set[i] - dmx_cur[i];
+    dmx_cur[i] = dmx_cur[i] + diff;
+  }
 }
 
 void TaskNetwork(void *pvParameters)
 {
-  for(;;) {
+  for (;;)
+  {
     handlePacket();
   }
 }
 
-void loop() {
+void loop()
+{
   vTaskDelay(200);
   printTasks();
 }
 
 char ptrTaskList[250];
-void tasks(char *pcWriteBuffer );
+void tasks(char *pcWriteBuffer);
 
-void printTasks() {
-    tasks(ptrTaskList);
-    Serial.println(F("**********************************"));
-    Serial.print(ptrTaskList);
+void printTasks()
+{
+  tasks(ptrTaskList);
+  Serial.println(F("**********************************"));
+  Serial.print(ptrTaskList);
 }
 
-void tasks(char *pcWriteBuffer )
+void tasks(char *pcWriteBuffer)
 {
-TaskStatus_t *pxTaskStatusArray;
-volatile UBaseType_t uxArraySize, x;
-uint32_t ulTotalRunTime, ulStatsAsPercentage;
+  TaskStatus_t *pxTaskStatusArray;
+  volatile UBaseType_t uxArraySize, x;
+  uint32_t ulTotalRunTime, ulStatsAsPercentage;
 
-   /* Make sure the write buffer does not contain a string. */
-   *pcWriteBuffer = 0x00;
+  /* Make sure the write buffer does not contain a string. */
+  *pcWriteBuffer = 0x00;
 
-   /* Take a snapshot of the number of tasks in case it changes while this
+  /* Take a snapshot of the number of tasks in case it changes while this
    function is executing. */
-   uxArraySize = uxTaskGetNumberOfTasks();
+  uxArraySize = uxTaskGetNumberOfTasks();
 
-   /* Allocate a TaskStatus_t structure for each task.  An array could be
+  /* Allocate a TaskStatus_t structure for each task.  An array could be
    allocated statically at compile time. */
-   pxTaskStatusArray = (TaskStatus_t*) pvPortMalloc( uxArraySize * sizeof( TaskStatus_t ) );
+  pxTaskStatusArray = (TaskStatus_t *)pvPortMalloc(uxArraySize * sizeof(TaskStatus_t));
 
-   if( pxTaskStatusArray != NULL )
-   {
+  if (pxTaskStatusArray != NULL)
+  {
 
-      /* Generate raw status information about each task. */
-      uxArraySize = uxTaskGetSystemState( pxTaskStatusArray,
-                                 uxArraySize,
-                                 &ulTotalRunTime );
-      /* For percentage calculations. */
-      ulTotalRunTime /= 100UL;
-      /* Avoid divide by zero errors. */
-      if( ulTotalRunTime > 0 )
-      {
-         /* For each populated position in the pxTaskStatusArray array,
+    /* Generate raw status information about each task. */
+    uxArraySize = uxTaskGetSystemState(pxTaskStatusArray,
+                                       uxArraySize,
+                                       &ulTotalRunTime);
+    /* For percentage calculations. */
+    ulTotalRunTime /= 100UL;
+    /* Avoid divide by zero errors. */
+    if (ulTotalRunTime > 0)
+    {
+      /* For each populated position in the pxTaskStatusArray array,
          format the raw data as human readable ASCII data. */
-         for( x = 0; x < uxArraySize; x++ )
-         {
-            /* What percentage of the total run time has the task used?
+      for (x = 0; x < uxArraySize; x++)
+      {
+        /* What percentage of the total run time has the task used?
             This will always be rounded down to the nearest integer.
             ulTotalRunTimeDiv100 has already been divided by 100. */
-            ulStatsAsPercentage =
-                  pxTaskStatusArray[ x ].ulRunTimeCounter / ulTotalRunTime;
+        ulStatsAsPercentage =
+            pxTaskStatusArray[x].ulRunTimeCounter / ulTotalRunTime;
 
-            if( ulStatsAsPercentage > 0UL )
-            {
-               sprintf( pcWriteBuffer, "%s\t\t%lu\t\t%lu%%\r\n",
-                                 pxTaskStatusArray[ x ].pcTaskName,
-                                 pxTaskStatusArray[ x ].ulRunTimeCounter,
-                                 ulStatsAsPercentage );
-            }
-            else
-            {
-               /* If the percentage is zero here then the task has
+        if (ulStatsAsPercentage > 0UL)
+        {
+          sprintf(pcWriteBuffer, "%s\t\t%lu\t\t%lu%%\r\n",
+                  pxTaskStatusArray[x].pcTaskName,
+                  pxTaskStatusArray[x].ulRunTimeCounter,
+                  ulStatsAsPercentage);
+        }
+        else
+        {
+          /* If the percentage is zero here then the task has
                consumed less than 1% of the total run time. */
-               sprintf( pcWriteBuffer, "%s\t\t%lu\t\t<1%%\r\n",
-                                 pxTaskStatusArray[ x ].pcTaskName,
-                                 pxTaskStatusArray[ x ].ulRunTimeCounter );
-            }
+          sprintf(pcWriteBuffer, "%s\t\t%lu\t\t<1%%\r\n",
+                  pxTaskStatusArray[x].pcTaskName,
+                  pxTaskStatusArray[x].ulRunTimeCounter);
+        }
 
-            pcWriteBuffer += strlen( ( char * ) pcWriteBuffer );
-         }
+        pcWriteBuffer += strlen((char *)pcWriteBuffer);
       }
+    }
 
-      /* The array is no longer needed, free the memory it consumes. */
-      vPortFree( pxTaskStatusArray );
-   }
+    /* The array is no longer needed, free the memory it consumes. */
+    vPortFree(pxTaskStatusArray);
+  }
 }
 
-
-void handlePacket() {
+void handlePacket()
+{
   int packetSize = Udp.parsePacket();
-  if(packetSize)
+  if (packetSize)
   {
     IPAddress remote = Udp.remoteIP();
     uint16_t port = Udp.remotePort();
     // read the packet into packetBufffer
-    Udp.read(packetBuffer,UDP_TX_PACKET_MAX_SIZE);    
+    Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
     switch (packetBuffer[1])
     {
     case TLight:
       break;
     case TDim:
-      for(uint8_t i=0; i!=COUNT_OF(dimmers); i++) {
-        Dimmer &d=dimmers[i];
-        uint8_t id=packetBuffer[2];
-        if(d.nr==id){
+      for (uint8_t i = 0; i != COUNT_OF(dimmers); i++)
+      {
+        Dimmer &d = dimmers[i];
+        uint8_t id = packetBuffer[2];
+        if (d.nr == id)
+        {
           uint8_t chanNr = packetBuffer[3];
           uint8_t brightVal = packetBuffer[4];
-          if(chanNr==0)
-            d.valWW=brightVal;
-          if(chanNr==1)
-            d.valCW=brightVal;
+          if (chanNr == 0)
+            d.valWW = brightVal;
+          if (chanNr == 1)
+            d.valCW = brightVal;
           d.writeDMX();
           Udp.beginPacket(remote, port);
           Udp.write("OK");
           Udp.endPacket();
         }
-
       }
-      break; 
+      break;
     default:
-          Udp.beginPacket(remote, port);
-          Udp.write("NOK");
-          Udp.endPacket();
+      Udp.beginPacket(remote, port);
+      Udp.write("NOK");
+      Udp.endPacket();
       break;
     }
   }
