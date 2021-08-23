403028a4b36aab42fee706972c483a289644ac67
Martin Kunz <martin.michael.kunz@gmail.com>
2021-08-23 22:11:17 +0200

    parse value

--- src/mqtt.cpp
+++ src/mqtt.cpp
@@ -1,5 +1,6 @@
 #include <SPI.h>
 #include <string.h>
+#include <config.h>
 
 
 #include "mqtt.h"
@@ -28,8 +29,21 @@ const char* dimmerPath="home/light/dimmer/";
 void callbackMQTT(char* topic, byte* payload, unsigned int length) {
     if (strncmp(topic, dimmerPath, strlen(dimmerPath)-1)==0)
     {
-        char *idChar = topic+strlen(dimmerPath);
-        Serial.print(idChar);
+        uint8_t tLength = strlen(topic);
+        if(tLength <= strlen(dimmerPath))
+            return;
+        uint8_t id = atoi(topic + strlen(dimmerPath));
+        char valStr[length + 1];
+        valStr[length + 1] = '\n';
+        memcpy(valStr, payload, length);
+        uint8_t value = atoi(valStr);
+
+        for(uint8_t i=0;i<dimmerSize;i++) {
+            if(dimmer[i].id == id)
+                dimmer[i].value = value;
+                
+        }
+        Serial.print(value);
         Serial.println();
     }
     
